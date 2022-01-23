1d601e7afadd178677bef39b176043795c515b89
Pier4413 <pierre.delmas@delmasweb.net>
2022-01-23 17:50:49 +0100

    Ajout de differents fichiers
    Ajout fichier des villes. Ajout des requirements. Mise à jour du fichier main pour utiliser dotenv

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
+import os
+
 from typing import Any
+from dotenv import load_dotenv
+
 from classes.httpRequest.dataRequest import dataRequest as request
 from classes.element.position import Position as position
 from classes.element.temperature import Temperature as temp
 from classes.element.wind import Wind as wind
 from classes.element.weather import Weather as weather
 
-weatherData = weather(
-    wind=wind(), 
-    position=position(name="Nantes", country="FR", id=2990969, longitude=-1.5534, latitude=47.2173), 
-    temperature=temp(), 
-    pressure=0, 
-    humidity=0)
+load_dotenv()
+
+pos=position(name="Nantes", country="FR", id=2990969, longitude=-1.5534, latitude=47.2173)
 
 def requestWeather() -> Any:
-    rObject = request("605acfbb33d9e69764c03738c0b3fb49")
+    rObject = request(os.getenv('API_KEY'))
     try:
-        result = rObject.makeRequest(uri="https://api.openweathermap.org", url="data/2.5/weather", getParams="id="+str(weatherData.getPosition().getId()))
+        result = rObject.makeRequest(uri="https://api.openweathermap.org", url="data/2.5/weather", getParams="id="+str(pos.getId()))
         
-        # On set le vent
-        weatherData.getWind().setSpeed(result['wind']['speed'])
-        weatherData.getWind().setDirection(result['wind']['deg']) 
+        weatherData = weather(
+            wind=wind(speed=result['wind']['speed'], direction=result['wind']['deg']), 
+            position=pos, 
+            temperature=temp(current=result['main']['temp'], min=result['main']['temp_min'], max=result['main']['temp_max'], feelsLike=result['main']['feels_like']), 
+            pressure=result['main']['pressure'], 
+            humidity=result['main']['humidity'])
 
-        # On set la temperature
-        weatherData.getTemperature().setCurrent(result['main']['temp'])
-        weatherData.getTemperature().setMin(result['main']['temp_min'])
-        weatherData.getTemperature().setMax(result['main']['temp_max'])
-        weatherData.getTemperature().setFeelsLike(result['main']['feels_like'])
-
-        # On set la pression
-        weatherData.setPressure(result['main']['pressure'])
-
-        # On set l'humidite
-        weatherData.setHumidity(result['main']['humidity'])
-
-
-        print(weatherData)
+        return weatherData
     except:
         print("Probleme sur la requete de meteo")
 

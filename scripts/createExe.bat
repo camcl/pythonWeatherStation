56833c83bcdce9f9802307aee3fb44cdf625b574
Pier4413 <pierre.delmas@delmasweb.net>
2022-02-06 21:00:43 +0100

    Update the create script

--- scripts/createExe.bat
+++ scripts/createExe.bat
@@ -1,8 +1,6 @@
 @echo off
 
 pyinstaller -F -w --icon="resources/images/icon.ico" main.py
-mkdir .\dist\resources
-mkdir .\dist\conf
 xcopy /E .\resources\ .\dist\resources\
 xcopy /E .\conf\ .\dist\conf\
 mkdir .\dist\logs
